ca1234b839ebdbf7c11ba529f62d8f02534011ec
kvapehe <33888633+kvapehe@users.noreply.github.com>
2021-11-13 02:26:11 +0100

    Levert

--- main.ts
+++ main.ts
@@ -1,1 +1,31 @@
-
+input.onButtonPressed(Button.A, function () {
+    tall += 1
+})
+let tall = 0
+basic.showNumber(tall)
+basic.forever(function () {
+    if (tall >= 10) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . # . .
+            . # . # .
+            . . . . .
+            `)
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
+            `)
+        tall += 1
+        basic.pause(1)
+    } else {
+        basic.showNumber(tall)
+    }
+    // Vise en X først og så starte på nytt med neste klikk.
+    if (tall >= 11) {
+        tall = 0
+    }
+})
